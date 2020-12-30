521f3b2aa3f5bdd510dafce5501492282f18f26d
pavitra <pavitraperumal77@gmail.com>
2020-12-30 12:31:31 +0530

    commit

---
+++ Asssignment-2/queue.go
@@ -0,0 +1,29 @@
+package main
+import "fmt"
+
+func enqueue(queue[] int, element int) []int {
+  queue = append(queue, element); 
+  fmt.Println("Enqueued:", element);
+  return queue
+}
+
+func dequeue(queue[] int) ([]int) {
+  element := queue[0]; 
+  fmt.Println("Dequeued:", element)
+  return queue[1:]; 
+
+func main() {
+  var queue[] int; // Make a queue of ints.
+
+  queue = enqueue(queue, 10);
+  queue = enqueue(queue, 20);
+  queue = enqueue(queue, 30);
+
+  fmt.Println("Queue:", queue);
+
+  queue = dequeue(queue);
+  fmt.Println("Queue:", queue);
+
+  queue = enqueue(queue, 40);
+  fmt.Println("Queue:", queue);
+}
