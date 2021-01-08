5b3733ab02425cfb8575b9edcc2480311ba0aee7
pavitra <pavitraperumal77@gmail.com>
2021-01-08 11:59:47 +0530

    commit

---
+++ Assignment-4/http.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(W http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(W, "Welcome to my home page\n", r.URL.Path[1:])
+}
+func handler2(W http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(W, "hello world\n")
+}
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
